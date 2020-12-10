e70746e08ec7aa2241d025a8bc1cf15db241d8aa
heyu <1395294694@qq.com>
2020-12-10 20:05:00 +0800

    feat: 乘注释

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,11 @@ export function divide(a: number, b: number): number {
     return a / b;
 }
 
+/**
+ * 乘
+ * @param a 
+ * @param b 
+ */
 export function multiply(a: number, b: number): number {
     return a * b;
 }
